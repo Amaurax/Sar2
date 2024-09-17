6a0a2228acc070459d403eb54039dc203d41a832
Amaury <goguillot.amaury@gmail.com>
2024-09-17 23:56:31 +0200

    ajout test serveur echo

---
+++ task1/task1/tests/echo/EchoClient.java
@@ -0,0 +1,31 @@
+package task1.tests.echo;
+import java.io.IOException;
+
+public class EchoClient extends Task {
+    public EchoClient(Broker broker) {
+    	//Runnable à définir dans echotest de préférdnce ?
+        super(broker, new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Channel channel = broker.connect("SharedBroker", 3); // Connexion au serveur
+                    byte[] sequence = new byte[255];
+                    for (int j = 0; j < 255; j++) {
+                        sequence[j] = (byte) (j + 1);
+                    }
+                    channel.write(sequence, 0, sequence.length); // Envoyer la séquence de bytes
+                    byte[] echo = new byte[255];
+                    int bytesRead = channel.read(echo, 0, echo.length); // Lire la réponse
+                    for (int j = 0; j < bytesRead; j++) {
+                        if (echo[j] != sequence[j]) {
+                            System.out.println("Erreur d'echo à l'octet : " + j);
+                        }
+                    }
+                    channel.disconnect(); // Déconnexion une fois la communication terminée
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
